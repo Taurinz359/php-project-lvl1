d5ad2ea42e77b603bc10a232fefdaf835cce4209
Gadel <gadel.g@myhart.ru>
2022-07-15 12:22:46 +0300

    [3] Сторонние библиотеки

---
+++ src/Cli.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function run(): void
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May i have your name?');
+    line("Hello, {$name}");
+}
+
